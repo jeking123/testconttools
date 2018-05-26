62d5a5a42aaf0c2ccfd0e2980e83a8305a3ce7c2
jeking123 <22380569+jeking123@users.noreply.github.com>
2018-05-25 21:21:59 -0700

    is content editor tools working yet?

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,12 @@
 // Your Scripts
 console.log('Hey Browser!');
+
+window.addEventListener('load', function() {
+    var editor;
+    ContentTools.StylePalette.add([
+        new ContentTools.Style('Author', 'author', ['p'])
+    ]);
+});
+
+editor = ContentTools.EditorApp.get();
+editor.init('*[data-editable]', 'data-name');
